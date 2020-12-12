e1d5efd099fed0ad4cf422a700507781311071a2
Chase Clark <chase_clark07@hotmail.com>
2020-12-12 14:45:01 -0500

    add hitbox and hurtbox and restart button

--- World/Grass.gd
+++ World/Grass.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 var GrassEffect = load("res://Effects/GrassEffect.tscn")
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var grass_effect = GrassEffect.instance()
-		var world = get_tree().current_scene
-		grass_effect.global_position = global_position
-		world.add_child(grass_effect)
-		queue_free()
+func create_grass_effect():
+	var grass_effect = GrassEffect.instance()
+	var world = owner # or  get_tree().current_scene
+	grass_effect.global_position = global_position
+	world.add_child(grass_effect)	
+	
+func _on_HurtBox_area_entered(area):
+	create_grass_effect()
+	queue_free()
